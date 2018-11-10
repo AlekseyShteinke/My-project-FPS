b7ce86f329601c3ca84bd73ccb7a4f0034a10d60
AlekseyShteinke <43885149+AlekseyShteinke@users.noreply.github.com>
2018-11-10 21:15:05 +0300

    добавил замок

---
+++ Beginning of the End/Assets/skripts/NumberOfKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NumberOfKey : MonoBehaviour {
+    public Text scoreLabel;
+    public string itemName;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Item collected:" + itemName);
+        Destroy(this.gameObject);
+       
+    }
+    void Start()
+    {
+        float scoreLabel = 0f;
+    }
+    void Update()
+    {
+        scoreLabel = "1";
+    }
+   
+}
